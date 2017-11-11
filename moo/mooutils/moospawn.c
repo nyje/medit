d47a9f092191450afe1b99a4b539b8fdc1d3087d
Yevgen Muntyan <muntyan@fastmail.fm>
2017-11-11 15:51:29 -0800

    G_SPAWN_WIN32_HIDDEN_CONSOLE

--- moo/mooutils/moospawn.c
+++ moo/mooutils/moospawn.c
@@ -530,7 +530,7 @@ moo_cmd_run_command (MooCmd     *cmd,
     else
 #endif
     {
-        flags |= MOO_SPAWN_WIN32_HIDDEN_CONSOLE;
+        flags |= G_SPAWN_WIN32_HIDDEN_CONSOLE;
 
         if ((flags & G_SPAWN_STDOUT_TO_DEV_NULL) || (cmd_flags & MOO_CMD_STDOUT_TO_PARENT))
             outp = NULL;
